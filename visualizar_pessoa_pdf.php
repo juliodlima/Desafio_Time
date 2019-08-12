742b2aab7b92e3a6fc20b6feddc77841ebd10a0f
juliodlima <juliodlima@yahoo.com.br>
2019-08-12 19:49:16 -0300

    correcao pdf

--- visualizar_pessoa_pdf.php
+++ visualizar_pessoa_pdf.php
@@ -14,8 +14,19 @@ $idade = $linha['idade'];
 $endereco = $linha['endereco'];
 $telefone = $linha['telefone'];
 $celular = $linha['celular'];
-$comp_comportamentais = $linha['comp_comportamentais'];
-$comp_tecnicas = $linha['comp_tecnicas'];
+
+$query = "SELECT c.nome, c.tipo FROM competencias c JOIN pessoa_competencia pc ON (c.id_competencia = pc.id_competencia) WHERE pc.id_pessoa = $id_pessoa";
+$busca_pessoa_competencia = mysqli_query($conexao, $query);
+
+$campo_comportamentais = '';
+$campo_tecnicas = '';
+while($linha = mysqli_fetch_array($busca_pessoa_competencia)){
+    if($linha['tipo'] == 'Comportamental') {
+        $campo_comportamentais.= $linha['nome'].',';
+    } else if($linha['tipo'] == 'Tecnica') {
+        $campo_tecnicas.= $linha['nome'].',';
+    }
+}
 
 $data_inscricao = date('d/m/Y',strtotime($linha['data_inscricao']));
 
@@ -23,9 +34,6 @@ date_default_timezone_set('America/Sao_Paulo');
 $date = date('d/m/Y H:i');
 
 require_once __DIR__ . '/vendor/autoload.php';
-
-
-
 
 $html = "
     <fieldset>
@@ -36,8 +44,8 @@ $html = "
     <p>Data de Nascimento: <strong>$data_nascimento</strong><nbsp> Idade: <strong>$idade</strong></p>
     <p>Telefone Residencial: <strong>$telefone</strong><nbsp> Celular: <strong>$celular</strong></p>
     <p>Endereço: <strong>$endereco</strong></p>
-    <p>Competências Comportamentais: <strong>$comp_comportamentais</strong></p>
-    <p>Competências Técnicas: <strong>$comp_tecnicas</strong></p>
+    <p>Competências Comportamentais: <strong>$campo_comportamentais</strong></p>
+    <p>Competências Técnicas: <strong>$campo_tecnicas</strong></p>
     <br>
     <p class='center'>Certificamos que <strong>$nome_pessoa</strong> está inscrito(a) desde o dia $data_inscricao no Time Inovação.</p>
     <br>
@@ -55,6 +63,7 @@ $mpdf->AddPage('L');
 $css = file_get_contents("css/estilo_pdf.css");
 $mpdf->WriteHTML($css,1);
 $mpdf->WriteHTML($html);
+
 $mpdf->Output("certificado_$date.pdf","I");
 
 exit;
